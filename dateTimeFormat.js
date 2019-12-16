46a4a4e14fbb7b937142c4c4876883b7e799ffc6
Kevin <liukaiwen-cn@outlook.com>
2019-12-16 14:55:16 +0800

    init

---
+++ dateTimeFormat.js
@@ -0,0 +1,41 @@
+function timeFormat(time, cFormat){
+    /**
+ * 时间格式化
+ * @param time (date or 时间戳)
+ * @param cFormat
+ * @returns {*}
+ * */
+
+    if (arguments.length === 0) {
+      return null
+    }
+    const format = cFormat || '{y}-{m}-{d} {h}:{i}:{s}'
+    let date
+    if (typeof time === 'object') {
+      date = time
+    } else {
+      if (('' + time).length === 10) time = parseInt(time) * 1000
+      date = new Date(time)
+    }
+    const formatObj = {
+      y: date.getFullYear(),
+      m: date.getMonth() + 1,
+      d: date.getDate(),
+      h: date.getHours(),
+      i: date.getMinutes(),
+      s: date.getSeconds(),
+      a: date.getDay()
+    }
+    // eslint-disable-next-line
+    const time_str = format.replace(/{(y|m|d|h|i|s|a)+}/g, (result, key) => {
+      let value = formatObj[key]
+      if (key === 'a') return ['一', '二', '三', '四', '五', '六', '日'][value - 1]
+      if (result.length > 0 && value < 10) {
+        value = '0' + value
+      }
+      return value || 0
+    })
+    // eslint-disable-next-line
+    return time_str
+
+}
